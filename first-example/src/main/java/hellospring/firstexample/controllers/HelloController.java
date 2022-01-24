54d8b9c74f2fa17aff7c625d30629ac1483a056e
jihoonk-1116 <jihoonk1116@gmail.com>
2022-01-24 17:40:35 -0500

    welcome page and HelloController

---
+++ first-example/src/main/java/hellospring/firstexample/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package hellospring.firstexample.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","Hello from Controller");
+        return "hello";
+    }
+}
