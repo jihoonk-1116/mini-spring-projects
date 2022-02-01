29ce551dab63ec2d107cc25a7c869b654dc9a1af
jihoonk-1116 <jihoonk1116@gmail.com>
2022-01-31 21:01:24 -0500

    Implemented Member domain and MemberRepository to manage member's data on system memory. Tested Repository methods by Junit test method

---
+++ first-example/src/main/java/hellospring/firstexample/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hellospring.firstexample.repository;
+
+import hellospring.firstexample.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //In case of Null, use Optional to handle null data in service class
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //if it's null, return it as Optional type
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); //Return store map's all member
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
---
+++ first-example/src/test/java/hellospring/firstexample/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,66 @@
+package hellospring.firstexample.repository;
+
+import hellospring.firstexample.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//TDD
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repo = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repo.clearStore();
+    }
+    //Invoke this method after done each test
+    //Erase dependencies between each test method
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repo.save(member);
+
+        Member result = repo.findById(member.getId()).get();
+        //Assertions.assertEquals(member, result);
+        assertThat(result).isEqualTo(member);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repo.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repo.save(member2);
+
+        Member result = repo.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repo.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repo.save(member2);
+
+        List<Member> result = repo.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
