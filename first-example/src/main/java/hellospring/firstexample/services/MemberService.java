a2d745b6b417319574f15a3e8f0b7282ea6c0cf3
jihoonk-1116 <jihoonk1116@gmail.com>
2022-02-01 19:01:59 -0500

    Implemented MemberService and its test unit. Utilized Dependency Injection during testing.

---
+++ first-example/src/main/java/hellospring/firstexample/services/MemberService.java
@@ -0,0 +1,47 @@
+package hellospring.firstexample.services;
+
+import hellospring.firstexample.domain.Member;
+import hellospring.firstexample.repository.MemberRepository;
+import hellospring.firstexample.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+
+
+        checkDuplicatedMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+    private void checkDuplicatedMember(Member member) {
+
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("Already Existed Member");
+            });
+        // Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent( m -> { //Member object is included in Optional object
+//            throw new IllegalStateException("Already Existed Member...");
+//        });
+    }
+
+}
---
+++ first-example/src/test/java/hellospring/firstexample/services/MemberServiceTest.java
@@ -0,0 +1,67 @@
+package hellospring.firstexample.services;
+
+import hellospring.firstexample.domain.Member;
+import hellospring.firstexample.repository.MemberRepository;
+import hellospring.firstexample.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+        //Dependency Injection to use same repository during testing
+    }
+
+
+    @AfterEach
+    public void afterEach(){
+
+    }
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member testMember = memberService.findOne(saveId).get();
+        //It's because findOne() returns Optional
+        assertThat(member.getName()).isEqualTo(testMember.getName());
+        //
+    }
+
+    @Test
+    void testDuplicatedMember() {
+
+        Member member1 = new Member();
+        member1.setName("test");
+
+        Member member2 = new Member();
+        member2.setName("test");
+
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        //Occurring exception is expected when the left logic is performed.
+
+//        IllegalStateException event = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        assertThat(event.getMessage()).isEqualTo("Already Existed Member");
+
+    }
+
+    @Test
+    void findOne() {
+    }
+}
