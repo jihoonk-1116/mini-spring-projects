e53f6ef012742771bc063087396f593da283d904
jihoonk-1116 <jihoonk1116@gmail.com>
2022-02-01 23:00:53 -0500

    Implemented createForm that routes to createMemberForm HTML file with GET method, createMember that gather user's name and create Member with POST method in MemberController.

--- first-example/src/main/java/hellospring/firstexample/controllers/MemberController.java
+++ first-example/src/main/java/hellospring/firstexample/controllers/MemberController.java
@@ -1,8 +1,14 @@
 package hellospring.firstexample.controllers;
 
+import hellospring.firstexample.domain.Member;
 import hellospring.firstexample.services.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //Component Scan
 public class MemberController {
@@ -16,4 +22,26 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     } //DI by Constructor
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return  "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
 }
