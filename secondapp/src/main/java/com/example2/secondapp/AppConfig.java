c565d8dec96823ae30efe5f73c8383758945aa9a
jihoonk-1116 <jihoonk1116@gmail.com>
2022-02-18 22:18:13 -0500

    AppConfig refactoring

--- secondapp/src/main/java/com/example2/secondapp/AppConfig.java
+++ secondapp/src/main/java/com/example2/secondapp/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example2.secondapp;
 
+import com.example2.secondapp.discount.DiscountPolicy;
 import com.example2.secondapp.discount.FixDiscountPolicy;
+import com.example2.secondapp.discount.RateDiscountPolicy;
 import com.example2.secondapp.member.MemberService;
 import com.example2.secondapp.member.MemberServiceImpl;
 import com.example2.secondapp.member.MemoryMemberRepository;
@@ -15,11 +17,20 @@ import com.example2.secondapp.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }
 
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy getDiscountPolicy(){
+        //Switch policy
+        return new RateDiscountPolicy();
+        //return new FixDiscountPolicy();
+    }
 }
