97fbc5f29b804dce103c1c30288ddb8e715f4b96
jihoonk-1116 <jihoonk1116@gmail.com>
2022-02-24 22:50:35 -0500

    Practiced using Lombok's setter, getter and constructor.

--- secondapp/src/main/java/com/example2/secondapp/order/OrderServiceImpl.java
+++ secondapp/src/main/java/com/example2/secondapp/order/OrderServiceImpl.java
@@ -3,10 +3,13 @@ package com.example2.secondapp.order;
 import com.example2.secondapp.discount.DiscountPolicy;
 import com.example2.secondapp.member.Member;
 import com.example2.secondapp.member.MemberRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //Lombok annotation
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -18,12 +21,12 @@ public class OrderServiceImpl implements OrderService {
         it doesn't close to change of code. And, OrderService depends on both of DiscountPolicy and its object
         The object has to be injected by Spring using config class
     */
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        //Now, Both of DIP, OCP are followed. OrderServiceiImpl only depends on interfaces.
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        //Now, Both of DIP, OCP are followed. OrderServiceiImpl only depends on interfaces.
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     @Override
